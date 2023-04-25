2f0d12ae6d31bff01abda927ade9555d35ad635a
DimonUA94 <128724174+DimonUA94@users.noreply.github.com>
2023-04-25 19:05:31 +0300

    u

--- secondlesson/print_2.py
+++ secondlesson/print_2.py
@@ -3,6 +3,12 @@ f=open("print_2.py", "r")
 sum=0
 for i in range(1,13):
   sum+=1
-for line  in f:
-	print(line)
+lines = f.readlines()
+lines = [line.rstrip('\n') for line in lines]
+print(len(lines))
+count=len(lines)
+for id, item in enumerate(lines):
+	id=count-1
+	print(lines[id])
+	count-=1
 f.close()
